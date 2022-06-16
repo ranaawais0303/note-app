70b15c70f585912663828b2ea82a2b30ac67eaa6
Rana_awais <z.manzoor@pioneerlogics.com>
2022-06-16 00:01:03 -0700

    css style module and media query

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import './Button.css';
+// import './Button.css';
+import style from "./Button.module.css";
 
-const Button = props => {
+const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={style.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
